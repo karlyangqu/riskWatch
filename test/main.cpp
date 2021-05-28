85839db515c9e399510f5f03049fb6f0f0ace2da
Karl <karlyangqu@icloud.com>
2021-05-27 21:05:02 -0400

    initial

---
+++ test/main.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int fnumber;
+    
+    cout<<"hello"<<endl;
+    cin >> fnumber;
+    cout<< fnumber << endl;
+
+    /* code */
+    return 0;
+}
